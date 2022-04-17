d304804cb5e4c50a837df73dd518df83ded1ffe0
jekky <11986158+jac3km4@users.noreply.github.com>
2022-04-17 11:57:40 +0100

    Cleanup

--- src/lib.rs
+++ src/lib.rs
@@ -1,48 +1,9 @@
-// based on https://users.rust-lang.org/t/concatenate-const-strings/51712/5
-pub const fn combine<const L: usize>(a: &'static str, b: &'static str) -> [u8; L] {
-    let mut out = [0u8; L];
-    out = copy_slice(a.as_bytes(), out, 0);
-    out = copy_slice(b.as_bytes(), out, a.len());
-    out
-}
-
-pub const fn copy_slice<const L: usize>(
-    input: &[u8],
-    mut output: [u8; L],
-    offset: usize,
-) -> [u8; L] {
-    let mut index = 0;
-    loop {
-        output[offset + index] = input[index];
-        index += 1;
-        if index == input.len() {
-            break;
-        }
-    }
-    output
-}
-
-pub const fn take_n(input: &[u8], n: usize) -> &[u8] {
-    let mut index = input.len();
-    let mut slice = input;
-    loop {
-        match slice.split_last() {
-            None => return slice,
-            Some(_) if index == n => return slice,
-            Some((_, tail)) => {
-                slice = tail;
-                index -= 1;
-            }
-        }
-    }
-}
-
 #[macro_export]
-macro_rules! combine {
+macro_rules! const_combine {
     ($A:expr, $B:expr) => {
         unsafe {
             std::mem::transmute::<&[u8], &str>(
-                $crate::combine::<{ $A.len() + $B.len() }>($A, $B).as_slice(),
+                $crate::internal::combine::<{ $A.len() + $B.len() }>($A, $B).as_slice(),
             )
         }
     };
@@ -50,11 +11,11 @@ macro_rules! combine {
 
 /// useful when above doesn't work due to const expression limitations
 #[macro_export]
-macro_rules! combine_buf_sized {
+macro_rules! const_combine_bounded_with {
     ($A:expr, $B:expr, $max_size:expr) => {
         unsafe {
-            std::mem::transmute::<&[u8], &str>($crate::take_n(
-                &$crate::combine::<$max_size>($A, $B),
+            std::mem::transmute::<&[u8], &str>($crate::internal::take_n(
+                &$crate::internal::combine::<$max_size>($A, $B),
                 $A.len() + $B.len(),
             ))
         }
@@ -63,10 +24,58 @@ macro_rules! combine_buf_sized {
 
 /// does not work for strings larger than 1024 bytes
 #[macro_export]
-macro_rules! combine_buf {
+macro_rules! const_combine_bounded {
     ($A:expr, $B:expr) => {
-        $crate::combine_buf_sized!($A, $B, 1024)
+        $crate::const_combine_bounded_with!($A, $B, 1024)
     };
+}
+
+pub mod bounded {
+    pub use super::{
+        const_combine_bounded as const_combine,
+        const_combine_bounded_with as const_combine_with_len,
+    };
+}
+
+pub mod internal {
+    // based on https://users.rust-lang.org/t/concatenate-const-strings/51712/5
+    pub const fn combine<const L: usize>(a: &'static str, b: &'static str) -> [u8; L] {
+        let mut out = [0u8; L];
+        out = copy_slice(a.as_bytes(), out, 0);
+        out = copy_slice(b.as_bytes(), out, a.len());
+        out
+    }
+
+    pub const fn copy_slice<const L: usize>(
+        input: &[u8],
+        mut output: [u8; L],
+        offset: usize,
+    ) -> [u8; L] {
+        let mut index = 0;
+        loop {
+            output[offset + index] = input[index];
+            index += 1;
+            if index == input.len() {
+                break;
+            }
+        }
+        output
+    }
+
+    pub const fn take_n(input: &[u8], n: usize) -> &[u8] {
+        let mut index = input.len();
+        let mut slice = input;
+        loop {
+            match slice.split_last() {
+                None => return slice,
+                Some(_) if index == n => return slice,
+                Some((_, tail)) => {
+                    slice = tail;
+                    index -= 1;
+                }
+            }
+        }
+    }
 }
 
 #[cfg(test)]
@@ -75,17 +84,17 @@ mod tests {
     fn concat_strings() {
         const STR: &str = "abc";
 
-        assert_eq!(combine!(STR, "def"), "abcdef");
-        assert_eq!(combine!("def", STR), "defabc");
-        assert_eq!(combine!(STR, STR), "abcabc");
+        assert_eq!(const_combine!(STR, "def"), "abcdef");
+        assert_eq!(const_combine!("def", STR), "defabc");
+        assert_eq!(const_combine!(STR, STR), "abcabc");
     }
 
     #[test]
     fn concat_strings_sized() {
         const STR: &str = "abc";
 
-        assert_eq!(combine_buf!(STR, "def"), "abcdef");
-        assert_eq!(combine_buf!("def", STR), "defabc");
-        assert_eq!(combine_buf!(STR, STR), "abcabc");
+        assert_eq!(const_combine_bounded!(STR, "def"), "abcdef");
+        assert_eq!(const_combine_bounded!("def", STR), "defabc");
+        assert_eq!(const_combine_bounded!(STR, STR), "abcabc");
     }
 }
